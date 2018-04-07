704edd7be286c3d3d84dfe2d1d6f882ec4b18f8d
sam-eng <sme388@nyu.edu>
2018-04-07 16:06:13 -0400

    writes artists and album names to different files

--- pitchfork/spiders/albums.py
+++ pitchfork/spiders/albums.py
@@ -12,16 +12,19 @@ class AlbumsSpider(scrapy.Spider):
 		urls = [
 			'https://pitchfork.com/reviews/albums/popular/?page=1',
 			'https://pitchfork.com/reviews/albums/popular/?page=2',
+			'https://pitchfork.com/reviews/albums/popular/?page=3',
 		]
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
-		filename = response.url.split("/")[-2] + '.html'
+		album_file = 'album_names.txt'
+		artist_file = 'artists.txt'
 		album_names = response.css('.review__title-album').extract()
 		artists = response.css('.review__title-artist').extract()
-		with open(filename, 'w') as f:
+		with open(album_file, 'w') as al:
 			for i in album_names:
-				f.write(remove_html(i) + "\n")
+				al.write(remove_html(i) + "\n")
+		with open(artist_file, 'w') as art:
 			for j in artists:
-				f.write(remove_html(j) + "\n")
+				art.write(remove_html(j) + "\n")
