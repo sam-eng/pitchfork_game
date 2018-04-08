72d215d3d16b59717364bd57da7de14f68cf9395
sam-eng <sme388@nyu.edu>
2018-04-07 20:33:33 -0400

    important alterations; all scraped data is now written to the file

--- game.py
+++ game.py
@@ -4,19 +4,23 @@ import random
 def __main__():
 	artist_list = open("artists.txt").readlines()
 	album_list = open("album_names.txt").readlines()
-	score = 0; 
-	numbers = list(range(0, 12))
+	score = 0;
 
-	print("Hello! Welcome to a guessing game! Guess the artist of an album!")
+	print("Hello! Welcome to a guessing game! In this game, you'll be given the name of an album and you'll have to guess the artist.\n")
+	size = (int(input("How many questions would you like to have? The maximum is 60. ")))
 	
-	k = random.sample(numbers, 12)
-	for i in k:
-		print(album_list[k[i]] + "\n")
-		user_answer = input("Whose album is this?")
+	#generate a list of numbers from 0 to 59, inclusive
+	numbers = list(range(0, 60))
+	#create a list of of the user-given size, containing non-repeating numbers from 0 to 59, inclusive
+	k = random.sample(numbers, size)
+	for i in range(0,len(k)):
+		print(k)
+		print(album_list[k[i]])
+		user_answer = input("Whose album is this?\n")
 		if (user_answer.lower() == (artist_list[k[i]].lower()).replace("\n", "")):
 			score = score + 1	
 
-	print("Congrats! You got %d out of 12 correct!" % score)
+	print("Congrats! You got %d out of %d correct!" % (score, size))
 
 
 __main__()
--- pitchfork/spiders/albums.py
+++ pitchfork/spiders/albums.py
@@ -1,30 +1,37 @@
 import scrapy
 import re
 
+#removes html tags from scraped text by using regular expressions
 def remove_html(data):
 	p = re.compile(r'<.*?>')
 	return p.sub('', data)
 
 class AlbumsSpider(scrapy.Spider):
 	name = "albums"
+	open('album_names.txt', 'w').close()
+	open('artists.txt', 'w').close()
 
 	def start_requests(self):
+		#list of first five pages (first 60 albums) of most popular pitchfork album reviews
 		urls = [
 			'https://pitchfork.com/reviews/albums/popular/?page=1',
 			'https://pitchfork.com/reviews/albums/popular/?page=2',
-			#'https://pitchfork.com/reviews/albums/popular/?page=3',
+			'https://pitchfork.com/reviews/albums/popular/?page=3',
+			'https://pitchfork.com/reviews/albums/popular/?page=4',
+			'https://pitchfork.com/reviews/albums/popular/?page=5', 
 		]
 		for url in urls:
-			yield scrapy.Request(url=url, callback=self.parse)
+			yield scrapy.Request(url=url, callback=self.parse_albums)
 
-	def parse(self, response):
+	def parse_albums(self, response):
 		album_file = 'album_names.txt'
 		artist_file = 'artists.txt'
+		#image_file = 'covers.txt' 
 		album_names = response.css('.review__title-album').extract()
 		artists = response.css('.review__title-artist').extract()
-		with open(album_file, 'w') as al:
+		with open(album_file, 'a') as al:
 			for i in album_names:
 				al.write(remove_html(i) + "\n")
-		with open(artist_file, 'w') as art:
+		with open(artist_file, 'a') as art:
 			for j in artists:
 				art.write(remove_html(j) + "\n")
